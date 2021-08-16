6058f3fbda51a1112fa1686775e45bc3a04cdc4c
Esteban <gabbanaesteban@gmail.com>
2021-08-16 05:06:45 -0400

    Add swagger

--- api-service/src/app.js
+++ api-service/src/app.js
@@ -14,6 +14,16 @@ require('dotenv').config()
 const router = require('./routes/router')
 
 const swaggerDocument = yaml.load(fs.readFileSync(path.join(__dirname, '../docs/swagger.yml'), 'utf8'))
+const swaggerOptions = {
+  swaggerOptions: {
+    requestInterceptor: (req) => {
+      if (req.headers['Authorization'] && !req.headers['Authorization'].startsWith('Bearer ')) {
+        req.headers['Authorization'] = `Bearer ${req.headers['Authorization']}`
+      }
+      return req
+    }
+  }
+}
 
 const app = express()
 
@@ -22,7 +32,8 @@ app.use(cors())
 app.use(helmet())
 app.use(logger('dev'))
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions))
+
 app.use('/', router)
 
 module.exports = app
